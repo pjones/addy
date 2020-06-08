13658e766b802875ca84e057fe82df94b7c8f8f5
Peter Jones <pjones@devalot.com>
2020-06-08 13:53:12 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,35 +1,13 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
-, nix-hs ? import sources.nix-hs { inherit pkgs; }
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
-, ghc ? "default"
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
 nix-hs {
   cabal = ./addy.cabal;
   compiler = ghc;
 
-  overrides = lib: self: super: with lib; {
-    ghcide = import ghcide { ghc = compilerName; };
-
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-
-    ip = unBreak (dontCheck super.ip);
-    wide-word = unBreak (dontCheck (doJailbreak super.wide-word));
-
-    relude =
-      if super ? relude_0_6_0_0
-      then super.relude_0_6_0_0
-      else super.relude;
-
-      validation-selective =
-        dontCheck (
-          super.callCabal2nix
-          "validation-selective"
-          sources.validation-selective {});
-  };
+  overrides = lib: self: super:
+    with lib; {
+      ip = unBreak (dontCheck super.ip);
+      wide-word = unBreak (dontCheck (doJailbreak super.wide-word));
+    };
 }
